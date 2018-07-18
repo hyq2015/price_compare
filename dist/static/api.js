8c85cfdc5209edb15f15ff80849b384fbdef66d9
Ricky_Huang <huangyunqi2015>
2018-07-18 14:47:57 +0800

    test h5

---
+++ dist/static/api.js
@@ -0,0 +1,28 @@
+const URL={
+    indexConfig:{
+        url:'product/detail/',
+        method:'get'
+    },
+    login:{
+        url:'web/session/login',
+        method:'post'
+    },
+    indexData:{
+        url:'main/index/load',
+        method:'post'
+    },
+    morePageData:{
+        url:'main/more/load',
+        method:'post'
+    },
+    settingData:{
+        url:'main/setting/load',
+        method:'post'
+    },
+    rankData:{
+        url:'main/rank/load',
+        method:'post'
+    }
+
+};
+export default URL
---
+++ dist/styles.28c902fb.chunk.js
@@ -0,0 +1,27 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([[0], {
+    259: function (n, r, o) {
+        var i = o(562);
+        "string" == typeof i && (i = [[n.i, i, ""]]);
+        var t = {hmr: !0, transform: void 0, insertInto: void 0};
+        o(104)(i, t);
+        i.locals && (n.exports = i.locals)
+    }, 468: function (n, r, o) {
+        var i = o(469);
+        "string" == typeof i && (i = [[n.i, i, ""]]);
+        var t = {hmr: !0, transform: void 0, insertInto: void 0};
+        o(104)(i, t);
+        i.locals && (n.exports = i.locals)
+    }, 469: function (n, r, o) {
+        (n.exports = o(103)(!1)).push([n.i, "body {\n  margin: 0;\n  padding: 0;\n  font-family: sans-serif;\n  min-height: calc(100vh);\n}\n", ""])
+    }, 552: function (n, r, o) {
+        var i = o(553);
+        "string" == typeof i && (i = [[n.i, i, ""]]);
+        var t = {hmr: !0, transform: void 0, insertInto: void 0};
+        o(104)(i, t);
+        i.locals && (n.exports = i.locals)
+    }, 553: function (n, r, o) {
+        (n.exports = o(103)(!1)).push([n.i, "@media all and (min-width: 480px) {\r\n    .Signup {\r\n        padding: 60px 0;\r\n    }\r\n\r\n    .Signup form {\r\n        margin: 0 auto;\r\n        max-width: 320px;\r\n    }\r\n}\r\n\r\n.Signup form span.help-block {\r\n    font-size: 14px;\r\n    padding-bottom: 10px;\r\n    color: #999;\r\n}", ""])
+    }, 562: function (n, r, o) {
+        (n.exports = o(103)(!1)).push([n.i, ".NewNote form {\r\n    padding-bottom: 15px;\r\n}\r\n\r\n.NewNote form textarea {\r\n    height: 300px;\r\n    font-size: 24px;\r\n}", ""])
+    }
+}]);
